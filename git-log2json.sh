ed5c9fe4a02c3f399f9542c4b8234cd0f682f5dc
jamesturner-dev <james@jamesturner.dev>
2022-12-30 12:21:38 -0500

    git-log and git-stats to json

--- git-log2json.sh
+++ git-log2json.sh
@@ -1,24 +1,9 @@
 #!/usr/bin/env bash
 
-# OPTIONAL: use this stand-alone shell script to produce a JSON object
-# with information similar to git --stat.
-#
-# You can then easily cross-reference or merge this with the JSON git
-# log, since both are keyed on the commit hash, which is unique.
+# Use this one-liner to produce a JSON literal from the Git log:
 
 git log \
-    --numstat \
-    --format='%H' \
+    --pretty=format:'{%n  "commit": "%H",%n  "author": "%aN <%aE>",%n  "date": "%ad",%n  "message": "%f"%n},' \
     $@ | \
-    perl -lawne '
-        if (defined $F[1]) {
-            print qq#{"insertions": "$F[0]", "deletions": "$F[1]", "path": "$F[2]"},#
-        } elsif (defined $F[0]) {
-            print qq#],\n"$F[0]": [#
-        };
-        END{print qq#],#}' | \
-    tail -n +2 | \
-    perl -wpe 'BEGIN{print "{"}; END{print "}"}' | \
-    tr '\n' ' ' | \
-    perl -wpe 's#(]|}),\s*(]|})#$1$2#g' | \
-    perl -wpe 's#,\s*?}$#}#'
+    perl -pe 'BEGIN{print "["}; END{print "]\n"}' | \
+    perl -pe 's/},]/}]/'
